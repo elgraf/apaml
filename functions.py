d9116681b322e1e20cfc92e0f5eaa088a9b5b36d
ihatefrontend <nmorkotilo@gmail.com>
2018-01-16 11:10:12 +0200

    -- first commit

---
+++ functions.py
@@ -0,0 +1,199 @@
+__author__ = 'Artgor'
+
+import base64
+import os
+import uuid
+import random
+import numpy as np
+
+
+
+from codecs import open
+from PIL import Image
+from scipy.ndimage.interpolation import rotate, shift
+from skimage import transform
+
+from two_layer_net import FNN
+
+
+
+class Model(object):
+    def __init__(self):
+        """
+		Load weights for FNN here. Original weights are loaded from local folder, updated - from Amazon.
+		"""
+        self.params_original = np.load('models/original_weights.npy')[()]
+        self.params = np.load('models/updated_weights.npy')[()]
+
+    def process_image(self, image):
+        """
+		Processing image for prediction. Saving in temproral folder so that it could be opened by PIL. Cropping and scaling so that the longest side it 20. Then putting it in a center of 28x28 blank image. Returning array of normalized data.
+		"""
+
+        filename = 'digit' + '__' + str(uuid.uuid1()) + '.jpg'
+        with open('tmp/' + filename, 'wb') as f:
+            f.write(image)
+
+        img = Image.open('tmp/' + filename)
+
+        bbox = Image.eval(img, lambda px: 255 - px).getbbox()
+        if bbox == None:
+            return None
+        widthlen = bbox[2] - bbox[0]
+        heightlen = bbox[3] - bbox[1]
+
+        if heightlen > widthlen:
+            widthlen = int(20.0 * widthlen / heightlen)
+            heightlen = 20
+        else:
+            heightlen = int(20.0 * widthlen / heightlen)
+            widthlen = 20
+
+        hstart = int((28 - heightlen) / 2)
+        wstart = int((28 - widthlen) / 2)
+
+        img_temp = img.crop(bbox).resize((widthlen, heightlen), Image.NEAREST)
+
+        new_img = Image.new('L', (28, 28), 255)
+        new_img.paste(img_temp, (wstart, hstart))
+
+        imgdata = list(new_img.getdata())
+        img_array = np.array([(255.0 - x) / 255.0 for x in imgdata])
+        return img_array
+
+    def augment(self, image, label):
+        """
+		Augmenting image for training. Saving in temproral folder so that it could be opened by PIL. Cropping and scaling so that the longest side it 20. The width and height of the scaled image are used to resize it again so that there would be 4 images with different combinatins of weight and height. Then putting it in a center of 28x28 blank image. 12 possible angles are defined and each image is randomly rotated by 6 of these angles. Images converted to arrays and normalized. 
+		Returning these arrays and labels.
+		"""
+        filename = 'digit' + '__' + str(uuid.uuid1()) + '.jpg'
+        with open('tmp/' + filename, 'wb') as f:
+            f.write(image)
+
+        image = Image.open('tmp/' + filename)
+
+        ims_add = []
+        labs_add = []
+        angles = np.arange(-30, 30, 5)
+        bbox = Image.eval(image, lambda px: 255 - px).getbbox()
+
+        widthlen = bbox[2] - bbox[0]
+        heightlen = bbox[3] - bbox[1]
+
+        if heightlen > widthlen:
+            widthlen = int(20.0 * widthlen / heightlen)
+            heightlen = 20
+        else:
+            heightlen = int(20.0 * widthlen / heightlen)
+            widthlen = 20
+
+        hstart = int((28 - heightlen) / 2)
+        wstart = int((28 - widthlen) / 2)
+
+        for i in [min(widthlen, heightlen), max(widthlen, heightlen)]:
+            for j in [min(widthlen, heightlen), max(widthlen, heightlen)]:
+                resized_img = image.crop(bbox).resize((i, j), Image.NEAREST)
+                resized_image = Image.new('L', (28, 28), 255)
+                resized_image.paste(resized_img, (wstart, hstart))
+
+                angles_ = random.sample(set(angles), 6)
+                for angle in angles_:
+                    transformed_image = transform.rotate(np.array(resized_image), angle, cval=255,
+                                                         preserve_range=True).astype(np.uint8)
+                    labs_add.append(int(label))
+                    img_temp = Image.fromarray(np.uint8(transformed_image))
+                    imgdata = list(img_temp.getdata())
+                    normalized_img = [(255.0 - x) / 255.0 for x in imgdata]
+                    ims_add.append(normalized_img)
+        image_array = np.array(ims_add)
+        label_array = np.array(labs_add)
+        return image_array, label_array
+
+    # def load_weights_amazon(self, filename):
+    #     """
+		# Load weights from Amazon. This is npy. file, which neads to be read with np.load.
+		# """
+    #     s3 = boto3.client('s3', aws_access_key_id=os.environ['AWS_ACCESS_KEY_ID'],
+    #                       aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'])
+    #     s3.download_file('digit_draw_recognize', filename, os.path.join('tmp/', filename))
+    #     return np.load(os.path.join('tmp/', filename))[()]
+
+    def save_weights_amazon(self, filename, file):
+        """
+		Save weights to Amazon.
+		"""
+        REGION_HOST = 's3-external-1.amazonaws.com'
+        conn = S3Connection(os.environ['AWS_ACCESS_KEY_ID'], os.environ['AWS_SECRET_ACCESS_KEY'], host=REGION_HOST)
+        bucket = conn.get_bucket('digit_draw_recognize')
+        k = Key(bucket)
+        k.key = filename
+        k.set_contents_from_filename('tmp/' + filename)
+        return ('Weights saved')
+
+    def save_image(self, drawn_digit, image):
+        """
+		Save image on Amazon. Only existing files can be uploaded, so the image is saved in a temporary folder.
+		"""
+        filename = 'digit' + str(drawn_digit) + '__' + str(uuid.uuid1()) + '.jpg'
+        with open('tmp/' + filename, 'wb') as f:
+            f.write(image)
+
+        return ('Image saved successfully with the name {0}'.format(filename))
+
+    def predict(self, image):
+        """
+		Predicting image. If nothing is drawn, returns a message; otherwise 4 models are initialized and they make predictions.
+		They return a list of tuples with 3 top predictions and their probabilities. These lists are sent to "select_answer"
+		method to select the best answer. Also tuples are converted into strings for easier processing in JS. The answer and
+		lists with predictions and probabilities are returned.
+		"""
+
+        img_array = self.process_image(image)
+        if img_array is None:
+            return "Can't predict, when nothing is drawn"
+        net = FNN(self.params)
+        net_original = FNN(self.params_original)
+
+        top_3 = net.predict_single(img_array)
+        top_3_original = net_original.predict_single(img_array)
+
+        answer, top_3, top_3_original, = self.select_answer(top_3, top_3_original)
+
+        answers_dict = {'answer': str(answer), 'fnn_t': top_3, 'fnn': top_3_original}
+        # return answer, top_3, top_3_original
+        return answers_dict
+
+    def train(self, image, digit):
+        """
+		Models are trained. Weights on Amazon are updated.
+		"""
+        r = self.save_image(digit, image)
+        print(r)
+        net = FNN(self.params)
+        X, y = self.augment(image, digit)
+        net.train(X, y)
+
+        np.save('models/updated_weights.npy', net.params)
+
+        return ("Weights saved")
+
+    def select_answer(self, top_3, top_3_original):
+        """
+		Selects best answer from all. In fact only from the trained models, as they are considered to be better than untrained.
+		"""
+        answer = ''
+
+        if int(top_3[0][0]) == int(top_3_original[0][0]):
+            answer = str(top_3[0][0])
+        elif int(top_3[0][1]) < 50 and int(top_3_original[0][1]) < 50:
+            answer = "Can't recognize this as a digit"
+        elif int(top_3[0][0]) != int(top_3_original[0][0]):
+            if int(top_3[0][1]) > int(top_3_original[0][1]):
+                answer = str(top_3[0][0])
+            else:
+                answer = str(top_3_original[0][0])
+
+        top_3 = ['{0} ({1})%'.format(i[0], i[1]) for i in top_3]
+        top_3_original = ['{0} ({1})%'.format(i[0], i[1]) for i in top_3_original]
+        print(answer + " is")
+        return answer, top_3, top_3_original
---
+++ two_layer_net.py
@@ -0,0 +1,144 @@
+import numpy as np
+
+class FNN(object):
+	"""
+	A two-layer fully-connected neural network. The net has an input dimension of
+	N, a hidden layer dimension of H, and performs classification over C classes.
+	We train the network with a softmax loss function and L2 regularization on the
+	weight matrices. The network uses a ReLU nonlinearity after the first fully
+	connected layer.
+
+	In other words, the network has the following architecture:
+
+	input - fully connected layer - ReLU - fully connected layer - softmax
+
+	The outputs of the second fully-connected layer are the scores for each class.
+	"""
+
+	def __init__(self, weights, input_size=28*28, hidden_size=100, output_size=10):
+		"""
+		Initialize the model. Weights are passed into the class. Weights and biases are stored in the
+		variable self.params, which is a dictionary with the following keys:
+
+		W1: First layer weights; has shape (D, H)
+		b1: First layer biases; has shape (H,)
+		W2: Second layer weights; has shape (H, C)
+		b2: Second layer biases; has shape (C,)
+
+		Inputs:
+		- input_size: The dimension D of the input data.
+		- hidden_size: The number of neurons H in the hidden layer.
+		- output_size: The number of classes C.
+		"""
+		self.params = {}
+		self.params['W1'] = weights['W1']
+		self.params['b1'] = weights['b1']
+		self.params['W2'] = weights['W2']
+		self.params['b2'] = weights['b2']
+
+	def loss(self, X, y, reg=0.0):
+		"""
+		Compute the loss and gradients for a two layer fully connected neural
+		network.
+
+		Inputs:
+		- X: Input data of shape (N, D). Each X[i] is a training sample.
+		- y: Vector of training labels. y[i] is the label for X[i], and each y[i] is
+		  an integer in the range 0 <= y[i] < C. This parameter is optional; if it
+		  is not passed then we only return scores, and if it is passed then we
+		  instead return the loss and gradients.
+		- reg: Regularization strength.
+
+		Returns:
+		grads: Dictionary mapping parameter names to gradients of those parameters
+		  with respect to the loss function; has the same keys as self.params.
+		"""
+		# Unpack variables from the params dictionary
+		W1, b1 = self.params['W1'], self.params['b1']
+		W2, b2 = self.params['W2'], self.params['b2']
+		N, D = X.shape
+		# Compute the forward pass
+		l1 = X.dot(W1) + b1
+		l1[l1 < 0] = 0
+		l2 = l1.dot(W2) + b2
+		exp_scores = np.exp(l2)
+		probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+		
+		# Backward pass: compute gradients
+		grads = {}
+		probs[range(X.shape[0]), y] -= 1
+		dW2 = np.dot(l1.T, probs)
+		dW2 /= X.shape[0]
+		dW2 += reg * W2
+		grads['W2'] = dW2
+		grads['b2'] = np.sum(probs, axis=0, keepdims=True) / X.shape[0]
+		
+		delta = probs.dot(W2.T)
+		delta = delta * (l1 > 0)
+		grads['W1'] = np.dot(X.T, delta)/ X.shape[0] + reg * W1
+		grads['b1'] = np.sum(delta, axis=0, keepdims=True) / X.shape[0]
+
+		return grads
+
+	def train(self, X, y, learning_rate=0.1*(0.95**24)/32, reg=0.001, batch_size=24):
+		"""
+		Train this neural network using stochastic gradient descent.
+
+		Inputs:
+		- X: A numpy array of shape (N, D) giving training data.
+		- y: A numpy array f shape (N,) giving training labels; y[i] = c means that
+		  X[i] has label c, where 0 <= c < C.
+		- learning_rate: Scalar giving learning rate for optimization.
+		- reg: Scalar giving regularization strength.
+		- batch_size: Number of training examples to use per step.
+		"""
+		num_train = X.shape[0]
+
+		# Compute loss and gradients using the current minibatch
+		grads = self.loss(X, y, reg=reg)
+
+		self.params['W1'] -= learning_rate * grads['W1']
+		self.params['b1'] -= learning_rate * grads['b1'][0]
+		self.params['W2'] -= learning_rate * grads['W2']
+		self.params['b2'] -= learning_rate * grads['b2'][0]
+
+	def predict(self, X):
+		"""
+		Use the trained weights of this two-layer network to predict labels for
+		data points. For each data point we predict scores for each of the C
+		classes, and assign each data point to the class with the highest score.
+
+		Inputs:
+		- X: A numpy array of shape (N, D) giving N D-dimensional data points to
+		  classify.
+
+		Returns:
+		- y_pred: A numpy array of shape (N,) giving predicted labels for each of
+		  the elements of X. For all i, y_pred[i] = c means that X[i] is predicted
+		  to have class c, where 0 <= c < C.
+		"""
+		l1 = X.dot(self.params['W1']) + self.params['b1']
+		l1[l1 < 0] = 0
+		l2 = l1.dot(self.params['W2']) + self.params['b2']
+		exp_scores = np.exp(l2)
+		probs = exp_scores / np.sum(exp_scores, axis=1, keepdims=True)
+		y_pred = np.argmax(probs, axis=1)
+
+		return y_pred
+
+	def predict_single(self, X):
+		"""
+		Use the trained weights of this two-layer network to predict label for
+		data point. Works as predict method, but for a single image.
+		
+		Returns:
+		- top_3: a list of 3 top most probable predictions with their probabilities as tuples.
+		"""
+		l1 = X.dot(self.params['W1']) + self.params['b1']
+		l1[l1 < 0] = 0
+		l2 = l1.dot(self.params['W2']) + self.params['b2']
+		exp_scores = np.exp(l2)
+		probs = exp_scores / np.sum(exp_scores)
+		y_pred = np.argmax(exp_scores)
+		top_3 = list(zip(np.argsort(probs)[::-1][:3], np.round(probs[np.argsort(probs)[::-1][:3]] * 100, 2)))
+		return top_3
